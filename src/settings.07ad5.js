885f7937c2ad709313a77fd4c963714e152e58f2
C1oudust <amerkiousy@gmail.com>
2021-02-16 03:56:53 +0800

    build

---
+++ src/settings.07ad5.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"279e0",main:"b1daa"}};
